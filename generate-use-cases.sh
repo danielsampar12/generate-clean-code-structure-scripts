bed03e42c1cc7eacda5a4d8f7be41607e1419634
Daniel Sampar <daniel.sampar12@gmail.com>
2022-07-19 18:00:20 -0300

    Add try catch to gen:usecase

--- generate-use-cases.sh
+++ generate-use-cases.sh
@@ -10,4 +10,4 @@ mkdir -p ./src/modules/$1/useCases
 
 use_case="${arg^}UseCase"
 
-echo -e "export class $use_case {\n  async execute() {\n\n  }\n}" > "./src/modules/$arg/useCases/$use_case".ts
+echo -e "export class $use_case {\n  async execute() {\n    try {\n    } catch(error: any) {\n      throw new Error(error);\n    }\n  }\n}" > "./src/modules/$arg/useCases/$use_case".ts
